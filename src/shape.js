c62cbb1e56e4366408b843b7472cc43af8e96fee
cveti911 <tsveti.911@gmail.com>
2025-02-28 16:45:29 +0000

    Shapes change depending on quadrant.

--- src/shape.js
+++ src/shape.js
@@ -7,9 +7,31 @@ export default function Shape({id, groupId}){
     const {attributes, listeners, setNodeRef, transform} = useDraggable({
         id,
       });
+    
+    return (
+        <><div>
+            {groupId === "Q1" &&
+                <span className="shape" ref={setNodeRef} {...listeners} {...attributes}
+                    style={{ display: "inline-block", cursor: "move" }}>◯</span>}
+        </div>
+        <div>
+            {groupId === "Q2" &&
+                <span className="shape" ref={setNodeRef} {...listeners} {...attributes}
+                    style={{ display: "inline-block", cursor: "move" }}>⬡</span>}
+        </div>
+        <div>
+            {groupId === "Q3" &&
+                <span className="shape" ref={setNodeRef} {...listeners} {...attributes}
+                    style={{ display: "inline-block", cursor: "move" }}>□</span>}
+        </div>
+        <div>
+            {groupId === "Q4" &&
+                <span className="shape" ref={setNodeRef} {...listeners} {...attributes}
+                    style={{ display: "inline-block", cursor: "move" }}>△</span>}
+        </div>
+        </>
 
-    return <span className="shape" ref={setNodeRef} {...listeners} {...attributes}
-        style={{ display: "inline-block", cursor: "move"}}>◯</span>
+    )
 }
 
 
